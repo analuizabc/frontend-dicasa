ccfad995fbb988941a451e2f35fda580c0552a20
Leticia Resende <leticiaresende.estudo@gmail.com>
2022-05-13 07:42:01 -0300

    Pequenos ajustes página login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -17,31 +17,22 @@ function Login() {
         <div className="fundo">
             <div className="base">
                 <div className="container">
-                 <h1 className="noteTitle">Login</h1>
+                <h1 className="noteTitle">Login</h1>
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label className="texto">Email:</Form.Label>
-                         <Form.Control 
-                         type="email" 
-                         placeholder="" 
-                         onChange={(e)=>setEmail(e.target.value)}
-                         />
-                         <Form.Text className="text-muted">
-                         </Form.Text>
+                        <Form.Label className="texto">Email:</Form.Label>
+                         <Form.Control type="email" placeholder="" onChange={(e)=>setEmail(e.target.value)}/>
+                         <Form.Text className="text-muted"></Form.Text>
                      </Form.Group>
-                     <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
                          <Form.Label className="texto">Senha:</Form.Label>
-                         <Form.Control 
-                         type="password" 
-                         placeholder="" 
-                         onChange={(e)=>setPassword(e.target.value)} 
-                            />
-                        </Form.Group>
-                             Não tem login? Faça seu cadastro aqui
-                        <div className="inputs">
+                         <Form.Control type="password" placeholder="" onChange={(e)=>setPassword(e.target.value)} />
+                    </Form.Group>
+                            <h2 className="texto2">Não tem login? Faça seu cadastro aqui</h2>
+                            <div className="inputs">
                             <button onClick={login}>Acessar</button>
-                        </div>
-                 </Form>
+                            </div>
+                    </Form>
                 </div>
             </div>
         </div>
