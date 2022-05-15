807b8d4c1f079716c4a726c4a17e14d6258b648f
Ana Coelho <anacoelho@cpejr.com.br>
2022-05-15 08:58:47 -0300

    Continuação cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -7,28 +7,69 @@ function Cadastro() {
     const history=useHistory();
     return ( 
         <div className="base">
-            <div className="container">
-            <h1>Cadastre-se</h1>
-                <Form> 
-                             
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Endereço de E-mail</Form.Label>
-                        <Form.Control type="email" placeholder="" />
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                        </Form.Group>
+            <div className="containerCadastro">
+                <div>
+                    <h1>Cadastre-se</h1>
+                </div>
+                <div>
+                    <Form> 
+                        <div>
+                            <Form.Group className="campoFormulario" controlId="formBasicNomeDeUsuario">
+                            <Form.Label className="campoLabel">Nome de usuário:</Form.Label>
+                            <Form.Control className="campoCaixa"type="text" placeholder="" />
+                            <Form.Text className="textCampo">
+                            </Form.Text>
+                            </Form.Group>
+                        </div>    
+                        
+                        <div>
+                            <Form.Group className="campoFormulario" controlId="formBasicEmail">
+                            <Form.Label className="campoLabel">Endereço de E-mail:</Form.Label>
+                            <Form.Control className="campoCaixa" type="email" placeholder="" />
+                            <Form.Text className="textCampo">
+                            </Form.Text>
+                            </Form.Group>
+                        </div>  
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Senha:</Form.Label>
-                        <Form.Control type="password" placeholder="" />
-                        </Form.Group>
+                        <div>
+                            <Form.Group className="campoFormulario" controlId="formBasicPassword">
+                            <Form.Label className="campoLabel">Senha:</Form.Label>
+                            <Form.Control className="campoCaixa" type="password" placeholder="" />
+                            </Form.Group>
+                        </div> 
+
+                        <div>
+                            <Form.Group className="campoFormulario" controlId="formBasicPassword">
+                            <Form.Label className="campoLabel">Confirmar senha:</Form.Label>
+                            <Form.Control className="campoCaixa" type="password" placeholder="" />
+                            </Form.Group>
+                        </div> 
+
+                        <div>
+                            <Form.Group className="campoFormulario" controlId="formBasicCidade">
+                            <Form.Label className="campoLabel">Cidade:</Form.Label>
+                            <Form.Control className="campoCaixa"type="text" placeholder="" />
+                            <Form.Text className="textCampo">
+                            </Form.Text>
+                            </Form.Group>
+                        </div>    
+
+                        <div>
+                            <Form.Group className="campoFormulario" controlId="formBasicEndereço">
+                            <Form.Label className="campoLabel">Endereço:</Form.Label>
+                            <Form.Control className="campoCaixa"type="text" placeholder="" />
+                            <Form.Text className="textCampo">
+                            </Form.Text>
+                            </Form.Group>
+                        </div>    
 
 
-                        <button>Cadastre-se</button>
-                <div className="inputs">
-                          
-                    </div>
-                </Form>
+                        <div className="botão">
+                            <button>Cadastre-se</button>  
+                        </div>
+
+                    </Form>
+                </div>
              </div>
          </div>
     );
