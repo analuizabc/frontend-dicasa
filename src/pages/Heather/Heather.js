7dcec16a79dd63e8af4f06e763548c603705fc87
mariana rabelo <marianarabelo@cpejr.com.br>
2022-05-15 03:08:38 -0300

    responsividade mobile finalizada(header, menu, home, cardapio)

--- src/pages/Heather/Heather.js
+++ src/pages/Heather/Heather.js
@@ -1,14 +1,20 @@
-import { AppBar, List, ListItem, ListItemText, Toolbar, Typography, Drawer } from "@mui/material"
-import React from "react";
+import { AppBar, List, ListItem, ListItemText, Toolbar, Typography, Drawer, IconButton } from "@mui/material"
+import React, { useState } from "react";
 import "./Heather.css";
-
+import { FiMenu } from "react-icons/fi"
 
 
 function Heather(props) {
-    
+    const [open, setOpen] = useState(false);
+
+    function handleDrawer(isOpen){
+        setOpen(isOpen);
+    }
+
     return (
-        <>
-        <div className="appbar">
+    <div>
+    <div>
+    <div className="appbar">
         <AppBar position="static">     
           <div className=" toolBar">
             <Toolbar className="toolBar">
@@ -47,29 +53,42 @@ function Heather(props) {
             </Toolbar>
             </div>    
         </AppBar>
-     {props.children}
-     </div>
     
-    <div >
-     <Drawer open={true} className="menu">
+    </div>
+    
+    <div className="menu">
+     <AppBar position="static">
+         <Toolbar className="toolBar">
+             <IconButton edge="start" aria-label="sanduiche" onClick={() => handleDrawer(!open)}>
+                 <FiMenu/>
+             </IconButton>
+             <div className="logomobile">
+             <img src="/images/LOGO3.png" alt="logodicasa" className="img3"></img>
+             </div>
+         </Toolbar>
+     </AppBar>
+     </div>
+     <div>
+     <Drawer open={open} onClose={() => handleDrawer(false)}  className="menu">
          <List className="drawer">
-             <ListItem button>
+             <ListItem button onClick={()=> { window.location.href="/home"}}>
              <img src="/images/iconecasa.png" alt="casa" className="img2"></img>   
              <Typography><h1 className="nomemenu">Home</h1></Typography>
              </ListItem>
-             <ListItem button>
+             <ListItem button onClick={()=> { window.location.href="/cardapio"}}>
              <img src="/images/sacola2.png" alt="sacola" className="img2"/>   
              <Typography><h1 className="nomemenu">Produtos</h1></Typography>
              </ListItem>
-             <ListItem button>
+             <ListItem button onClick={()=> { window.location.href="/perfil"}}>
              <img src="/images/iconeperfil.png" alt="perfil" className="img2"/>
              <Typography><h1 className="nomemenu">Perfil</h1></Typography>
              </ListItem>   
-             
          </List>
      </Drawer>
-     </div>
-     </>
+    </div>
+</div>
+ {props.children}
+</div>
     );
 }
 
