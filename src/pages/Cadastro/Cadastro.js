25abfccd4cfb4e23091a849c031d576fd160ea6b
Ana Coelho <anacoelho@cpejr.com.br>
2022-05-14 21:56:47 -0300

    Início página de cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Form, Button} from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import "./Cadastro.css"
 
 function Cadastro() {
-    return <h1>Cadastro</h1>;
+    const history=useHistory();
+    return ( 
+        <div className="base">
+            <div className="container">
+            <h1>Cadastre-se</h1>
+                <Form> 
+                             
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Endereço de E-mail</Form.Label>
+                        <Form.Control type="email" placeholder="" />
+                        <Form.Text className="text-muted">
+                        </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Senha:</Form.Label>
+                        <Form.Control type="password" placeholder="" />
+                        </Form.Group>
+
+
+                        <button>Cadastre-se</button>
+                <div className="inputs">
+                          
+                    </div>
+                </Form>
+             </div>
+         </div>
+    );
 }
 
 export default Cadastro;
+
