ba66eedebb28e66e404cd6adaecd2ebd8a110128
Ana Coelho <anacoelho@cpejr.com.br>
2022-05-15 12:08:44 -0300

    Adicionando heather

--- src/routes.js
+++ src/routes.js
@@ -7,16 +7,18 @@ import Login from "./pages/Login";
 import Perfil from "./pages/Perfil";
 import Heather from "./pages/Heather";
 
+
 function Routes() {
     return(
         <BrowserRouter>
           <Switch>
-              <Route exact path="/cadastro" component={Cadastro}/>
-              <Route exact path="/login" component={Login}/>
-              <Route exact path="/perfil" component={Perfil}/>
               <Route path="/" component={UserHeather}/>
               <Route exact path="/home" component={Home}/>
               <Route exact path="/cardapio" component={Cardapio}/>
+              <Route exact path="/cadastro" component={Cadastro}/>
+              <Route exact path="/perfil" component={Perfil}/>
+              <Route exact path="/login" component={Login}/>
+
           </Switch>
         </BrowserRouter>
     )
@@ -28,9 +30,10 @@ function UserHeather() {
             <Switch>
                 <Route exact path="/cardapio" component={Cardapio}/>
                 <Route exact path="/home" component={Home}/>
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/cadastro" component={Cadastro}/>
             </Switch>
         </Heather>
     );
 }
-
 export default Routes;
