3eb1458e01b8ffe72f5a392ce7beabe272c0d934
mariana rabelo <marianarabelo@cpejr.com.br>
2022-05-16 00:48:45 -0300

    responsividade corrigida

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -4,10 +4,10 @@ import "./Note.css";
 
 
 function Note({ note }) {
-    return <div className="note">
+    return <div className="Note">
         <img src={note.src} alt="carda" className="produtos"></img>
-        <div className="noteTitle"><text className="nome">{note.title}</text></div>
-        <div className="noteDescription"><text className="valor">{note.description}</text></div>
+        <div className="notetitle"><text className="nome">{note.title}</text></div>
+        <div className="notedescription"><text className="valor">{note.description}</text></div>
         <button className="button" id=""><h4 className="selecionar">Selecionar</h4></button>{' '}
     </div>;
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -33,14 +33,14 @@ function Home() {
         </Carousel>
         </div>
         <div className="container">
-            <div className="notetitle"><Typography><h1 className="titulo">Quem somos</h1></Typography></div>
-            <div className="notedescription"><Typography><h1 className="texto">A ‘DiCasa’ Lanches Saudáveis surgiu com a proposta de levar saúde, sabor e qualidade, de maneira criativa e por meio de alimentos naturais e livres de conservantes. Somos uma cozinha especializada em alimentação saudável com foco em sanduíches naturais, saladas, sucos naturais, vitaminas, tapiocas e crepiocas. Todos os nossos produtos são inspirados em receitas caseiras e livres de aditivos como conservantes e temperos industriais. 
+            <div className="notetitlE"><Typography><h1 className="titulo">Quem somos</h1></Typography></div>
+            <div className="Notedescription"><Typography><h1 className="textO">A ‘DiCasa’ Lanches Saudáveis surgiu com a proposta de levar saúde, sabor e qualidade, de maneira criativa e por meio de alimentos naturais e livres de conservantes. Somos uma cozinha especializada em alimentação saudável com foco em sanduíches naturais, saladas, sucos naturais, vitaminas, tapiocas e crepiocas. Todos os nossos produtos são inspirados em receitas caseiras e livres de aditivos como conservantes e temperos industriais. 
             </h1></Typography></div>
-            <div className="notetitle"><Typography><h1 className="titulo">Nossa história</h1></Typography></div>
-            <div className="notedescription"><Typography><h1 className="texto">Fundada em 2022, a ‘DiCasa’ é fruto da paixão da fundadora, Maria Júlia Pimenta, pela culinária e alimentação saudável. Ao se mudar e perceber que em sua nova cidade só havia restaurantes e lanchonetes especializadas em fast-food, Maria Pimenta sentiu a necessidade de criar um projeto em que qualidade, saúde e sabores caseiros e naturais fossem priorizados. 
+            <div className="notetitlE"><Typography><h1 className="titulo">Nossa história</h1></Typography></div>
+            <div className="Notedescription"><Typography><h1 className="textO">Fundada em 2022, a ‘DiCasa’ é fruto da paixão da fundadora, Maria Júlia Pimenta, pela culinária e alimentação saudável. Ao se mudar e perceber que em sua nova cidade só havia restaurantes e lanchonetes especializadas em fast-food, Maria Pimenta sentiu a necessidade de criar um projeto em que qualidade, saúde e sabores caseiros e naturais fossem priorizados. 
             </h1></Typography></div>
-            <div className="notetitle"><Typography><h1 className="titulo">Nosso objetivo</h1></Typography></div>
-            <div className="notedescription"><Typography><h1 className="texto">Nosso objetivo é transformar a visão das pessoas sobre alimentação e culinária, unindo o saudável ao saboroso. Nosso foco é trazer receitas tradicionais, adicionando ao cardápio de inúmeras pessoas, refeições caseiras e naturais. Comer bem importa! 
+            <div className="notetitlE"><Typography><h1 className="titulo">Nosso objetivo</h1></Typography></div>
+            <div className="Notedescription"><Typography><h1 className="textO">Nosso objetivo é transformar a visão das pessoas sobre alimentação e culinária, unindo o saudável ao saboroso. Nosso foco é trazer receitas tradicionais, adicionando ao cardápio de inúmeras pessoas, refeições caseiras e naturais. Comer bem importa! 
             </h1></Typography></div>  
         </div>
         <div className="linhaStyle"></div>
