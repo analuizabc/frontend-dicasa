0d1746de5a6003187a7e320d7de6a2391c9be0f4
Ana Coelho <anacoelho@cpejr.com.br>
2022-05-18 00:35:08 -0300

    Revert "Merge branch 'main' into Cadastro"
    This reverts commit 3b22cb5c2986ec845d62f0eae5eae995ccd3475a.

--- src/pages/Cardapio/Cardapio.js
+++ src/pages/Cardapio/Cardapio.js
@@ -4,80 +4,79 @@ import "./Cardapio.css"
 import Note from "../../components/Note";
 
 
-
 const notes = [
     {
         id: 1,
         src:"/images/Rectangle61.png",
         title: "Combo Sanduíche + Suco",
-        description: "R$ 20,00",
+        description: "R$20,00",
         
     },
     {
         id: 2,
-        src:"/images/Rectangle30.png",
+        src:"/images/Rectangle62.png",
         title: "Tapioca",
-        description: "R$ 10,00",
+        description: "R$10,00",
     },
     {
         id: 3,
         src:"/images/Rectangle63.png",
         title: "Crepioca",
-        description: "R$ 12,00",
+        description: "R$12,00",
     },
     {
         id: 4,
         src:"/images/Rectangle64.png",
         title: "Pastel Assado",
-        description: "R$ 9,00",
+        description: "R$9,00",
     },
     {
         id: 5,
         src:"/images/Rectangle65.png",
         title: "Sanduíche Natural",
-        description: "R$ 15,00",
+        description: "R$15,00",
     },
     {
         id: 6,
         src:"/images/Rectangle66.png",
         title: "Salada de Frutas",
-        description: "R$ 10,00",
+        description: "R$10,00",
     },
     {
         id: 7,
         src:"/images/Rectangle67.png",
         title: "Salada Caesar",
-        description: "R$ 15,00",
+        description: "R$15,00",
     },
     {
         id: 8,
         src:"/images/Rectangle68.png",
         title: "Saalada Tropical",
-        description: "R$ 12,00",
+        description: "R$12,00",
     },
     {
         id: 9,
         src:"/images/Rectangle69.png",
         title: "Omelete",
-        description: "R$ 13,50",
+        description: "13,50",
     },
     {
         id: 10,
         src:"/images/Rectangle70.png",
         title: "Suco Verde",
-        description: "R$ 8,00",
+        description: "R$8,00",
     },
     {
         id: 11,
         src:"/images/Rectangle71.png",
         title: "Suco Natural",
-        description: "R$ 6,00",
+        description: "R$6,00",
     },
     {
         id: 12,
         src:"/images/Rectangle72.png",
         title: "Vitamina",
-        description: "R$ 9,00",
+        description: "R$9,00",
     },
 ]
 
@@ -85,15 +84,14 @@ function Cardapio() {
     return (
      <div className="cardapio">
         <img src="/images/Cardapio.png" alt="logocardapio" className="logocarda"></img>
-        <Input type="text"  className="barra"></Input>
-       
+        <Input type="text" placeholder=""></Input>
+
         <div className="noteContainer">
             {notes.map((note) => (
             <Note key={note.id} note={note} />
             ))}
 
         </div>
-        <img src="/images/logo2.png" alt="logofinal" className="logofinal"></img>
      </div>
 
     );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,43 +1,32 @@
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import React from "react";
+import { Form } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import "./Login.css"
 
 
 function Login() {
     const history=useHistory();
-    const [email, setEmail]=useState();
-    const [password, setPassword]=useState();
-
-    function login(){
-        history.push("home");
-    }
-    function cadastro(){
-        history.push("cadastro");
-    }
     return (
-        <div className="fundo">
-            <div className="base">
-                <div className="Container">
-                <h1 className="noteTitle">Login</h1>
+        <div className="base">
+            <div className="container">
+                <button onClick={()=>{history.push("/home");}}>Home</button>
+                <h1>Login</h1>
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label className="texto">E-mail:</Form.Label>
-                    <Form.Control className="caixa" type="email" placeholder="" onChange={(e)=>setEmail(e.target.value)}/>
-                    <Form.Text className="text-muted"></Form.Text>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="" />
+                        <Form.Text className="text-muted">
+                        </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label className="texto">Senha:</Form.Label>
-                    <Form.Control type="password" placeholder="" onChange={(e)=>setPassword(e.target.value)} />
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password" placeholder="" />
                     </Form.Group>
-                        <h2 className="texto2">Não tem login? Faça seu cadastro
-                        <button onClick={cadastro} className='botaolink'> aqui</button>
-                        </h2>
-                        <div className="inputs">
-                        <button onClick={login} className='botao'>Acessar</button>
-                        </div>
+                         Não tem login? Faça seu cadastro aqui
+                    <div className="inputs">
+                        <button>Acessar</button>
+                    </div>
                 </Form>
-                </div>
             </div>
         </div>
     );
