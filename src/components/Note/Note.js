1bdda4d70df522406ad80bd0f231fad1d69391b0
Leticia Resende <leticiaresende.estudo@gmail.com>
2022-05-20 06:37:22 -0300

    Segunda parte página perfil

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,15 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
 import "./Note.css";
 
 
-function Note({ note }) {
+function Note({ note,props }) {
+    console.log(props);
     return <div className="Note">
         <img src={note.src} alt="carda" className="produtos"></img>
         <div className="notetitle"><text className="nome">{note.title}</text></div>
         <div className="notedescription"><text className="valor">{note.description}</text></div>
-        <button className="button" id=""><h4 className="selecionar">Selecionar</h4></button>{' '}
+        {props ? (
+            <button className="button" id=""><h4 className="selecionar">Selecionar</h4></button>
+        ) : (
+            <></>
+        )
+    }
+        
     </div>;
 }
+
+
 
 export default Note;
--- src/pages/Perfil/Perfil.js
+++ src/pages/Perfil/Perfil.js
@@ -3,6 +3,85 @@ import React, { useState } from "react";
 import "./Perfil.css"
 import { Typography, Input} from "@mui/material";
 import { Form, Button} from "react-bootstrap";
+import Note from "../../components/Note";
+
+const notes = [
+    {
+        id: 1,
+        src:"/images/Rectangle61.png",
+        title: "Combo Sanduíche + Suco",
+        description: "R$ 20,00",
+        
+    },
+    {
+        id: 2,
+        src:"/images/Rectangle30.png",
+        title: "Tapioca",
+        description: "R$ 10,00",
+    },
+    {
+        id: 3,
+        src:"/images/Rectangle63.png",
+        title: "Crepioca",
+        description: "R$ 12,00",
+    },
+    {
+        id: 4,
+        src:"/images/Rectangle64.png",
+        title: "Pastel Assado",
+        description: "R$ 9,00",
+    },
+    {
+        id: 5,
+        src:"/images/Rectangle65.png",
+        title: "Sanduíche Natural",
+        description: "R$ 15,00",
+    },
+    {
+        id: 6,
+        src:"/images/Rectangle66.png",
+        title: "Salada de Frutas",
+        description: "R$ 10,00",
+    },
+    {
+        id: 7,
+        src:"/images/Rectangle67.png",
+        title: "Salada Caesar",
+        description: "R$ 15,00",
+    },
+    {
+        id: 8,
+        src:"/images/Rectangle68.png",
+        title: "Saalada Tropical",
+        description: "R$ 12,00",
+    },
+    {
+        id: 9,
+        src:"/images/Rectangle69.png",
+        title: "Omelete",
+        description: "R$ 13,50",
+    },
+    {
+        id: 10,
+        src:"/images/Rectangle70.png",
+        title: "Suco Verde",
+        description: "R$ 8,00",
+    },
+    {
+        id: 11,
+        src:"/images/Rectangle71.png",
+        title: "Suco Natural",
+        description: "R$ 6,00",
+    },
+    {
+        id: 12,
+        src:"/images/Rectangle72.png",
+        title: "Vitamina",
+        description: "R$ 9,00",
+    },
+]
+
+
 
 
 function Perfil() {
@@ -10,7 +89,7 @@ function Perfil() {
     const [Cidade, setCidade]=useState();
     const [EndereçodeEmail, setEndereçodeEmail]=useState();
     const [Endereço, setEndereço]=useState();
-    
+    const [botaoVisivel, setBotaoVisivel]=useState(false);
     return (
         <div className="Perfil">
             <Typography><h1 className="NoteTitle_perfil">Perfil</h1></Typography>
@@ -63,10 +142,19 @@ function Perfil() {
                 </div>
             </div>
             <Typography><h1 className="texto_perfil">Itens selecionados: </h1></Typography>
-            <div className="Retangulo">
+            {/* <div className="Retangulo"> */}
+                <div className="noteContainer_perfil">
+                    {notes.map((note) => (
+                        <div className="produto_perfil">
+                            <Note key={note.id} note={note} props={botaoVisivel} />
+                        </div>
+                    ))}
+                </div>
+            {/* </div> */}
 
-            </div>
-            <img src="/images/Cardapio.png" alt="logocardapio" className="logocarda"></img>
+            <img src="/images/logo2.png" alt="logofinal" className="logofinal"></img>
+            
+            
         </div>
 
     );
