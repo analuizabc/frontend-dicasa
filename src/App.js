d80b1cd45fa29a7aa9695d56a67029d64a6ff02f
Ana Coelho <anacoelho@cpejr.com.br>
2022-05-22 11:26:32 -0300

    continuação footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import Routes from "./routes";
 import "./global.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Heather from "./pages/Heather";
+import Footer from "./pages/Footer";
 function App() {
-  return <Routes/>;
+  return (
+  <>
+    <Heather/>
+      <Routes/>
+    <Footer/>
+  </>
+);
+
 }
 
 export default App;
--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,8 +1,14 @@
+import { AppBar, List, ListItem, ListItemText, Toolbar, Typography, } from "@mui/material"
 import React from "react";
+import { IconContext } from "react-icons/lib";
+import { FaHome } from "react-icons/fa";
+
+import "./Footer.css";
 
 function Footer(){
 
-    return(<h1>Teste</h1>);
+return
+   (<h1>teste</h1>);
 }
 
 export default Footer;
