764a31983117e0b8fabb43cbb7d11ff2891ede59
Ana Coelho <anacoelho@cpejr.com.br>
2022-05-25 20:05:18 -0300

    ajustes ao footer

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -6,50 +6,69 @@ import "./Footer.css";
 
 function Footer(props) {
 return( 
-   
-    <div className="paginaFooter">
-        
-                 <div className="containerFooter" >  
-                    <ListItemText className="listItemText">         
-                        <Typography><h1 className="text1">Telefone: 31 9 9593-1173</h1></Typography>
-                    </ListItemText>
-
-                    <ListItemText className="listItemText">         
-                        <Typography><h1 className="text2">E-mail: dicasa@lanchessaudaveis.com.br</h1></Typography>
-                    </ListItemText>
-
-                    <ListItemText className="listItemText">         
-                        <Typography><h1 className="text3">Endereço: Av Antônio Carlos, 6627, Belo Horizonte - MG</h1></Typography>
-                    </ListItemText>
-                    
-                <div className=" icons1">
-                      
-                      <img src="/images/Whatsapp.png" className="imgWhatsapp"/>
-                      <img src="/images/E-mail.png" className="imgEmail"/>
-                      <img src="/images/Localização.png" className="imgLocal"/>
-
-                </div>
-                </div>  
-                    <div className="icons">
-
-
-                <a href="https://www.facebook.com/Dicasa-Lanches-saud%C3%A1veis-100846982624432"><img src="/images/Facebook.png" className="imgFacebook"/>
-                </a>   
-            
-                  <a href="https://twitter.com/DicasaLanches"><img src="/images/twitter.png" className="imgTwitter"/>
-                  </a>  
-                
-                <a href="https://www.instagram.com/dicasa.lanchessaudaveis/"><img src="/images/instagram.png" className="imgInstagram"/>
-                </a>
-                    </div>
-      
-
-
-                         <img src="/images/Line 1.png" className="imgLinha"/>
     
-    </div>
-      
-     
+        <div className="paginaFooter">
+            
+                    <div className="containerFooter" >  
+
+                        <div className="containerEsquerda">
+                            
+                           
+                            <div className="iconeTexto1">
+
+                              
+                                <img src="/images/Whatsapp.png" className="imgWhatsapp"/>
+                                      
+                                   <h1 className="text1">Telefone: 31 9 9593-1173</h1>
+                                
+                             
+                            </div>
+                        
+                                <div className="iconeTexto1">
+
+                              
+                                    <img src="/images/E-mail.png" className="imgEmail"/>
+                                        
+                                        <h1 className="text1">E-mail: dicasa@lanchessaudaveis.com.br</h1>
+                                
+                            
+                                
+                                </div>  
+
+                                    <div className="iconeTexto1">
+                                       
+                                        <img src="/images/Localização.png" className="imgLocal"/>
+                                            
+                                        <h1 className="text1">Endereço: Av. Antônio Carlos, 6627, Belo Horizonte - MG</h1>
+                                        
+                                      
+                                    </div>
+                        </div>
+
+
+                        <div className="containerDireita">
+                    
+            
+                            <a href="https://www.facebook.com/Dicasa-Lanches-saud%C3%A1veis-100846982624432"target="_black"><img src="/images/Facebook.png" className="imgFacebook"/> 
+                                </a>   
+                                
+                                    <a href="https://twitter.com/DicasaLanches"target="_black"><img src="/images/twitter.png" className="imgTwitter"/>
+                                        </a>  
+                                            
+                                            <a href="https://www.instagram.com/dicasa.lanchessaudaveis/"target="_black"><img src="/images/instagram.png" className="imgInstagram"/>
+                                                </a>
+
+                                
+                        </div>
+
+
+        </div>
+
+                    
+        
+     </div>
+        
+        
    
  
    
